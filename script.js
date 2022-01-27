9756d41c97310bec8b287eadec47bcf2de186fa0
Nutt Chairatana <nuttchai@outlook.com>
2022-01-27 23:04:40 +0700

    add home section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Background animation
 VANTA.NET({
-  el: "#home",
+  el: "#animation",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
@@ -8,7 +8,24 @@ VANTA.NET({
   minWidth: 200.0,
   scale: 1.0,
   scaleMobile: 1.0,
-  color: "#393d4a",
+  color: "#990000",
   backgroundColor: "#252934",
   points: 18.0,
+  showDots: false,
+});
+
+// home section
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 });
