786631bbf2f032a69c82ac7c6c716179162bdf32
Nutt Chairatana <nuttchai@outlook.com>
2022-01-27 19:58:19 +0700

    add navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+// Background animation
+VANTA.NET({
+  el: "#home",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: "#393d4a",
+  backgroundColor: "#252934",
+  points: 18.0,
+});
