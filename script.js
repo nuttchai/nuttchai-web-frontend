6351918b402bd7194f8b16cdb1101f3c3b0b2ac2
Nutt Chairatana <nuttchai@outlook.com>
2022-11-30 00:45:11 +0700

    hide the page to prepare dark mode styling

--- script.js
+++ script.js
@@ -172,6 +172,56 @@ $(document).ready(function () {
     }
   });
   //#endregion
+
+  //#region TOGGLE DARK MODE
+  // check for saved 'darkMode' in localStorage
+  let darkMode = localStorage.getItem("darkMode");
+
+  const darkModeToggle = document.querySelector("#darkmode-toggle");
+
+  const enableDarkMode = () => {
+    // 1. Add the class to the body
+    document.body.classList.add("darkmode");
+    // 2. Update darkMode in localStorage
+    localStorage.setItem("darkMode", "enabled");
+  };
+
+  const disableDarkMode = () => {
+    // 1. Remove the class from the body
+    document.body.classList.remove("darkmode");
+    // 2. Update darkMode in localStorage
+    localStorage.setItem("darkMode", null);
+  };
+
+  const initPage = () => {
+    if (darkMode === "enabled") {
+      // If the user already visited and enabled darkMode
+      // start things off with it on
+      enableDarkMode();
+      darkModeToggle.checked = true;
+    } else {
+      disableDarkMode();
+      darkModeToggle.checked = false;
+    }
+  };
+
+  // When someone clicks the button
+  darkModeToggle.addEventListener("click", () => {
+    // get their darkMode setting
+    darkMode = localStorage.getItem("darkMode");
+
+    // if it not current enabled, enable it
+    if (darkMode !== "enabled") {
+      enableDarkMode();
+      // if it has been enabled, turn it off
+    } else {
+      disableDarkMode();
+    }
+  });
+  //#endregion
+
+  initPage();
+  document.getElementsByTagName("html")[0].style.visibility = "visible";
 });
 
 //#region REMOVE CONTACT CURVE
@@ -186,47 +236,4 @@ $(window)
     }
   })
   .resize();
-//#endregion
-
-//#region TOGGLE DARK MODE
-// check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem("darkMode");
-
-const darkModeToggle = document.querySelector("#darkmode-toggle");
-
-const enableDarkMode = () => {
-  // 1. Add the class to the body
-  document.body.classList.add("darkmode");
-  // 2. Update darkMode in localStorage
-  localStorage.setItem("darkMode", "enabled");
-  // 3. Make checkbox checked if it doesn't
-  if (!darkModeToggle.checked) darkModeToggle.checked = true;
-};
-
-const disableDarkMode = () => {
-  // 1. Remove the class from the body
-  document.body.classList.remove("darkmode");
-  // 2. Update darkMode in localStorage
-  localStorage.setItem("darkMode", null);
-};
-
-// If the user already visited and enabled darkMode
-// start things off with it on
-if (darkMode === "enabled") {
-  enableDarkMode();
-}
-
-// When someone clicks the button
-darkModeToggle.addEventListener("click", () => {
-  // get their darkMode setting
-  darkMode = localStorage.getItem("darkMode");
-
-  // if it not current enabled, enable it
-  if (darkMode !== "enabled") {
-    enableDarkMode();
-    // if it has been enabled, turn it off
-  } else {
-    disableDarkMode();
-  }
-});
 //#endregion
